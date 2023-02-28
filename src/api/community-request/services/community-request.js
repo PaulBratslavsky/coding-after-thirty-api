4c7159bd90e38bdda3b4c851070ce4e686f16c8b
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-02-28 11:05:04 -0600

    testing email

--- src/api/community-request/services/community-request.js
+++ src/api/community-request/services/community-request.js
@@ -11,11 +11,11 @@ module.exports = createCoreService('api::community-request.community-request', (
     console.log("sendEmail called");
     await strapi.plugins['email'].services.email.send({
       to: 'paul.bratslavsky@gmail.com',
-      from: 'no-reply@strapi.io', //e.g. single sender verification in SendGrid
+      from: 'paul.bratslavsky@strapi.io', //e.g. single sender verification in SendGrid
       replyTo: 'codingafterthirty@gmail.com',
-      subject: 'The Strapi Email plugin worked successfully',
-      text: 'Hello world!',
-      html: 'Hello world!',
+      subject: 'You are the best!',
+      text: 'Hope you are having a great day!',
+      html: 'Hope you are having a great day!',
     });
   },
 }));
